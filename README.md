dab8fd1fc9f3149df703789cb8d03776894e15a7
alstocras <qzrnw@proton.me>
2026-08-20 16:33:53 +0000

    release: it is finally playable (#4)
    * WHAT HAVE I DONE
    
    * working incrementer
    
    * update readme
    
    * avoid issues
    
    * video
    
    * were doing gifs now
    
    * screenshots
    
    * finish readme
    
    * wording
    
    * quit

--- README.md
+++ README.md
@@ -3,3 +3,49 @@
 ![hackatime](https://hackatime.hackclub.com/api/v1/badge/U0BN4QJUWHK/alstocras/terminal-factory)
 
 a TUI factory management game
+
+hello! have you ever been sshed into a server or another pc, and wanted to play a factory management game?? but sadly, you dont have a graphical env!! no worries!! this horrendous terminal factory management should keep you entertained!!
+
+## video of intense gameplay
+
+![vid](demos/videos/main.gif)
+
+## exciting screenshots
+
+![startup](demos/shots/startup.png)
+![drill](demos/shots/drill.png)
+![core](demos/shots/core.png)
+![label](demos/shots/label.png)
+
+## keybinds
+
+| bind(s) | action |
+| ------- | ------ |
+| y, w | move up |
+| i, s | move down |
+| c, a | move left |
+| e, d | move right |
+| space | place drill |
+| q, ^c | quit |
+
+## how to play/run
+
+ok so
+
+cross compilation with c++ is really hard
+
+_...and ncurses is linux only. sorry!_
+
+so just download the `tf` binary from the repo, then cd next to it and run:
+
+``` ./tf ```
+
+enjoy!
+
+## misc stuff
+
+so this is actually my first time making a project in c++ without raylib, and it was really fun. ive used ncurses, a unix tui library, and it was kinda interesting to see how it does stuff.
+
+i like this. but tbh this turned out not to be such a good idea as i was limited to unicode on a grid.
+
+i made this bcs i was just really into tuis and mindustry when i started it, so i ended up combining the 2.
--- main.cpp
+++ main.cpp
@@ -12,6 +12,10 @@ int main() {
   // seed random gen
   srand(time(0));
 
+  // variables
+  int extractSpeed = 0;
+  float zeroes = 0;
+
   // ncurses screen stuff
   initscr();
   curs_set(0);
@@ -20,10 +24,11 @@ int main() {
 
   // colours!!
   start_color();
-  init_pair(1, COLOR_RED, COLOR_BLACK);
-  init_pair(2, COLOR_YELLOW, COLOR_BLACK);
-  init_pair(3, COLOR_GREEN, COLOR_BLACK);
-  init_pair(4, COLOR_BLACK, COLOR_RED);
+  init_pair(1, COLOR_RED, COLOR_BLACK);    // player
+  init_pair(2, COLOR_YELLOW, COLOR_BLACK); // drills
+  init_pair(3, COLOR_GREEN, COLOR_BLACK);  // ores
+  init_pair(4, COLOR_BLACK, COLOR_RED);    // core
+  init_pair(5, COLOR_BLACK, COLOR_CYAN);   // label
 
   // get centre
   int y, x;
@@ -45,8 +50,6 @@ int main() {
   for (int yb = 0; yb < map.size(); yb++) {
     for (int xb = 0; xb < map[yb].size(); xb++) {
       int randomNum = rand() % 50;
-      cout << randomNum;
-
       if (randomNum <= 25) {
         map[yb][xb] = 2;
       } else {
@@ -85,6 +88,16 @@ int main() {
   while ((ch = getch()) != 'q') {
     // game loop
 
+    // incrememnt resources
+    zeroes += extractSpeed;
+
+    // display 0s
+    // const char *zStr = ("zeroes: " + to_string(zeroes)).c_str();
+    attron(COLOR_PAIR(5));
+    mvwprintw(stdscr, 0, 0, to_string(zeroes).c_str());
+    refresh();
+    attroff(COLOR_PAIR(5));
+
     // erase player
     mvwprintw(stdscr, pos[0], pos[1], " ");
     refresh();
@@ -92,6 +105,15 @@ int main() {
     // movement!! (very janky ik)
     // up
     if ((ch = getch()) == 'y') {
+      pos[0] -= 1;
+
+      // render and refresh
+      attron(COLOR_PAIR(1));
+      mvwprintw(stdscr, pos[0], pos[1], "^");
+      refresh();
+      attroff(COLOR_PAIR(1));
+    }
+    else if ((ch = getch()) == 'w') {
       pos[0] -= 1;
 
       // render and refresh
@@ -110,8 +132,26 @@ int main() {
       refresh();
       attroff(COLOR_PAIR(1));
     }
+    else if ((ch = getch()) == 's') {
+      pos[0] += 1;
+
+      // render and refresh
+      attron(COLOR_PAIR(1));
+      mvwprintw(stdscr, pos[0], pos[1], "v");
+      refresh();
+      attroff(COLOR_PAIR(1));
+    }
     // left
     else if ((ch = getch()) == 'c') {
+      pos[1] -= 3;
+
+      // render and refresh
+      attron(COLOR_PAIR(1));
+      mvwprintw(stdscr, pos[0], pos[1], "<");
+      refresh();
+      attroff(COLOR_PAIR(1));
+    }
+    else if ((ch = getch()) == 'a') {
       pos[1] -= 3;
 
       // render and refresh
@@ -130,10 +170,22 @@ int main() {
       refresh();
       attroff(COLOR_PAIR(1));
     }
+    else if ((ch = getch()) == 'd') {
+      pos[1] += 3;
+
+      // render and refresh
+      attron(COLOR_PAIR(1));
+      mvwprintw(stdscr, pos[0], pos[1], ">");
+      refresh();
+      attroff(COLOR_PAIR(1));
+    }
 
     // drill placement check
     if ((ch = getch()) == ' ') {
-      map[pos[0]][pos[1]] = 1;
+      if (map[pos[0]][pos[1]] == 2) {
+        extractSpeed += 1;
+        map[pos[0]][pos[1]] = 1;
+      }
     }
 
     // drawing map
